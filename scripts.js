06085defbd8165d8ae6bf894f5a8c4143d9b28f2
cpdiego <cpdiego91@gmail.com>
2026-09-22 22:03:53 -0300

    Finalizada a criação do JS com, forEach, Map, Reduce e Filter

--- scripts.js
+++ scripts.js
@@ -1,14 +1,25 @@
 const menu = document.querySelector('ul')
 const buttonShowAll = document.querySelector('.show-all')
-let myLi = ''
+const buttonMapAll = document.querySelector('.map-all')
+const buttonSumAll = document.querySelector('.sum-all')
+const buttonFilterVegan = document.querySelector('.filter-vegan')
 
-function showAll() {
-    menuOptions.forEach((product) => {
+function formatCurrency(value) {
+    return newValue = value.toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+    })
+}
+
+function showAll(productsArray) {
+    let myLi = ''
+
+    productsArray.forEach((product) => {
         myLi += `
                     <li>
                         <img src=${product.src}>
                         <p>${product.name}</p>
-                        <p class="item-price">R$ ${product.price}</p>
+                        <p class="item-price">${formatCurrency(product.price)}</p>
                     </li>
              `
     })
@@ -16,4 +27,35 @@ function showAll() {
     menu.innerHTML = myLi
 }
 
-buttonShowAll.addEventListener('click', showAll)
+function mapAllItems() {
+    const newPrices = menuOptions.map((product) => ({
+        ...product,
+        price: product.price * 0.9,
+    }))
+
+    showAll(newPrices)
+}
+
+function sumAllItems() {
+    const totalValue = menuOptions.reduce((acc, curr) => acc + curr.price, 0)
+
+    menu.innerHTML = `
+        <li>
+            <p>A soma de todos <br>
+            os itens do <br> 
+            menu é <br>
+            ${formatCurrency(totalValue)}</p>
+        </li>
+        `
+}
+
+function filterAllVegan() {
+    const filterJustVegan = menuOptions.filter((product) => product.vegan)
+
+    showAll(filterJustVegan)
+}
+
+buttonShowAll.addEventListener('click', () => showAll(menuOptions))
+buttonMapAll.addEventListener('click', mapAllItems)
+buttonSumAll.addEventListener('click', sumAllItems)
+buttonFilterVegan.addEventListener('click', filterAllVegan)
